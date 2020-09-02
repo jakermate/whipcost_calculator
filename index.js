2be4ab7037214a1b9e65947668ac2a1fba2bc143
jm <root1@root.local>
2020-09-01 21:13:48 -0700

    New project structure with server index file.  Express server will serve from internal port 9001 on linux machine.

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const morgan = require('morgan')
+const PORT = process.env.PORT || 9001
+const server = express()
+const path = require('path')
+const cors = require('cors')
+
+server.use(morgan('common'))
+server.use(cors())
+server.use(express.static(path.join(__dirname, "build")))
+
+server.listen(()=>{
+    console.log('Whipcost running on internal port ' + PORT)
+})
