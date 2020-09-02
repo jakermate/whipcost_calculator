63efa63afb76a68a7dc122c74b9d342582a9561a
jm <root1@root.local>
2020-09-01 21:07:09 -0700

    Changed project structure to accommodate server.

---
+++ frontend/build/precache-manifest.07acd6b118a381eb6fa587194ef13ad1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93f919f9158677593156a55089e592f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29906017b4fce23bc461",
+    "url": "/static/css/main.a4aefb2e.chunk.css"
+  },
+  {
+    "revision": "1d656c280582159d50f7",
+    "url": "/static/js/2.8d7f46d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d7f46d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29906017b4fce23bc461",
+    "url": "/static/js/main.63896999.chunk.js"
+  },
+  {
+    "revision": "1a52ee89e60c90e942b6",
+    "url": "/static/js/runtime-main.c4b18110.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
